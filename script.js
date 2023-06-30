d10a506fd8c17b2b88e35fcfaa6c4d5b9eacd9e7
unknown <theonlymaxmc@gmail.com>
2023-06-30 12:22:29 -0700

    todo-Item

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const form = document.querySelector("myform");
+let text = document.getElementById("todo-input");
+let todoCon = document.querySelector(".todo-items");
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault;
+//     addItem();
+// })
+
 function addItem(event) {
-    let text = document.getElementById("todo-input");
-    
+    event.preventDefault();
+    let todoItems = document.createElement("div");
+    let inputText = text.value;
+    todoItems.innerHTML = `
+    <div class="todo-item">
+          <div class="check">
+            <div class="check-mark">
+              <img src="./images/icon-check.svg" alt="">
+            </div>
+          </div>
+          <div class="todo-text">${inputText}</div>
+        </div>
+    `
+    todoCon.appendChild(todoItems);
 }
