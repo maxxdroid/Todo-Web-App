242f01564dcf267fa79b1e2f38815bddf99dbc5b
unknown <theonlymaxmc@gmail.com>
2023-07-05 07:51:34 -0700

    todo-item-saved

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
-const form = document.querySelector("myform");
+// const form = document.querySelector("myform");
 let text = document.getElementById("todo-input");
 let todoCon = document.querySelector(".todo-items");
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault;
-//     addItem();
-// })
 
-function addItem(event) {
-    event.preventDefault();
+let todos = JSON.parse(localStorage.getItem("todos"));
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  addItem();
+})
+
+if (todos) {
+  todos.forEach(element => {
+    addItem(element);
+  })
+}
+
+function addItem(element) {
+    // event.preventDefault();
     let todoItems = document.createElement("div");
     let inputText = text.value;
+    if(element) {
+      inputText = element.text; 
+    }
+    if (inputText) {
     todoItems.innerHTML = `
     <div class="todo-item">
           <div class="check">
-            <div class="check-mark">
+            <div class="check-mark ${element && element.complete ? "checked":""}">
               <img src="./images/icon-check.svg" alt="">
             </div>
           </div>
-          <div class="todo-text">${inputText}</div>
+          <div class="todo-text ${element && element.complete ? "complete":""}">${inputText}</div>
         </div>
     `
     todoCon.appendChild(todoItems);
-    // createEventListeners();
-
+    updatels();}
     let check = todoItems.querySelector(".check-mark");
     let textcheck = todoItems.querySelector(".todo-text");
     check.addEventListener('click', () => {
       check.classList.add("checked");
-      textcheck.classList.add("checked");
-      // console.log(todoItems.children[0]);
+      textcheck.classList.add("complete");
+      updatels()
     })
+}
+
+function updatels() {
+  let textTag = document.querySelectorAll(".todo-text")
+  let arr=[];
+  textTag.forEach(element => {
+    arr.push({
+      text: element.innerText,
+      complete: element.classList.contains("complete")
+    })
+  })
+  localStorage.setItem("todos", JSON.stringify(arr));
 }
 
 function createEventListeners() {
